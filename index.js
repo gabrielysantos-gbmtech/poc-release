6c56b4a4da3e40e30371c7a98610003a33ee39e2
gabriely cristovao <gabriely.santos@gbmlogistica.com>
2025-12-26 15:00:40 -0300

    feat(api): get author

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get('/api/status', (req, res) => {
   res.status(200).json({ status: "Running", service: 'Single File API' });
 });
 
+app.get('/api/user', (req, res) => {
+  res.status(200).json({ user: "Gabriely" });
+});
+
 // Define a POST route (example of handling input)
 app.post('/api/data', (req, res) => {
   const receivedData = req.body;
