97ab25fa70da78a2ada1693f07f3cbd41846177b
gabrielysantos-gbmtech <gabriely.santos@gbmlogistica.com.br>
2025-12-29 09:37:20 -0300

    refactor: semantic release config

---
+++ release.config.js
@@ -0,0 +1,32 @@
+const branch = github.github_base_ref
+
+const config = {
+  branches: ['master', { name: 'dev', prerelease: true }, { name: 'develop', prerelease: true }],
+  "plugins": [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      [
+        "@semantic-release/github",
+        {
+          "assets": [
+            {
+              "path": "dist.zip",
+              "label": "Distribution"
+            }
+          ]
+        }
+      ]
+    ]
+}
+
+if (config.branches.some(it => it === branch || (it.name === branch && !it.prerelease))) {
+  config.plugins.push('@semantic-release/changelog', [
+    '@semantic-release/git',
+    {
+      assets: ['CHANGELOG.md'],
+      message: 'chore(release): ${nextRelease.version} \n\n${nextRelease.notes}',
+    },
+  ])
+}
+
+module.exports = config
